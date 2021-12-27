61d1eec5b336f414083f289d6f7fabe4bc387008
imsuhel <itssuhelcontact@gmail.com>
2021-12-27 19:12:17 +0530

    new changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import logo from "./logo.svg";
-import "./App.css";
+import HomePage from "./pages/HomePage";
 
 function App() {
   return (
     <div className="App">
-      <h5>jsdl</h5>
+      <HomePage />
     </div>
   );
 }
---
+++ src/pages/HomePage.js
@@ -0,0 +1,9 @@
+function HomePage() {
+  return (
+    <div>
+      <h5>home page</h5>
+    </div>
+  );
+}
+
+export default HomePage;
